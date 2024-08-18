fd8fba18497c9936891ac378eaea01410be33162
gooey goo <yourfriendtheuniversalgooeygoo@gmail.com>
2024-08-18 00:24:01 -0400

    Updated cinnamon forest biome

--- src/main/java/goocraft4evr/nonamedyes/world/biome/BiomeCinnamonForest.java
+++ src/main/java/goocraft4evr/nonamedyes/world/biome/BiomeCinnamonForest.java
@@ -7,6 +7,7 @@ import net.minecraft.core.world.biome.Biome;
 import net.minecraft.core.world.generate.feature.WorldFeature;
 import net.minecraft.core.world.generate.feature.tree.WorldFeatureTree;
 import net.minecraft.core.world.generate.feature.tree.WorldFeatureTreeFancyRainforest;
+import net.minecraft.core.world.generate.feature.tree.WorldFeatureTreePalm;
 import net.minecraft.core.world.generate.feature.tree.WorldFeatureTreeShrub;
 
 import java.util.Random;
@@ -18,10 +19,10 @@ public class BiomeCinnamonForest extends Biome {
 
 	@Override
     public WorldFeature getRandomWorldGenForTrees(Random random) {
-        if (random.nextInt(12) == 0) {
-            return new WorldFeatureTreeFancyRainforest(Block.leavesOak.id, Block.logOak.id, 0);
-        }
-        if (random.nextInt(6) == 0) {
+		if (random.nextInt(12) == 0) {
+			return new WorldFeatureTreePalm(Block.logPalm, Block.leavesPalm,false,false);
+		}
+        if (random.nextInt(4) == 0) {
             return new WorldFeatureTreeShrub(Block.leavesShrub.id, Block.logOak.id);
         }
         return new WorldFeatureTreeCinnamon(ModBlocks.leavesCinnamon.id, ModBlocks.logCinnamon.id);
